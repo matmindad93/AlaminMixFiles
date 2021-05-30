dc15ea73b07de5d5f347a3c5403dfab4bb5498c0
Alaminbd257 <67809938+Alaminbd257@users.noreply.github.com>
2021-05-30 05:01:24 +0300

    Update rjnet.sh

--- rjnet.sh
+++ rjnet.sh
@@ -932,7 +932,8 @@ cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 sed -i 's/#ForwardToWall=yes/ForwardToWall=no/g' /etc/systemd/journald.conf
 clear
 echo "Installation Done"
-udo apt-get clean > /dev/null 2>&1
+sudo apt-get autoremove -y > /dev/null 2>&1
+sudo apt-get clean > /dev/null 2>&1
 history -c
 cd /root || exit
 rm -f /root/installer.sh
